9d6d1bdfc7647d853ffd7dbad0cee2b875e3738e
Grey Larrain Bratt <greybratt@outlook.com>
2022-04-30 02:00:26 -0700

    html list of parks & accompanying placement css

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let requestOptions = {
 };
 
 const container = document.getElementById("container");
-
+let list = document.getElementById("parkList");
 
 const map = L.map("map").setView([37.703515, -121.973367], 8);
 L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png", {
@@ -29,7 +29,24 @@ fetch("https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=w0Ntsu0zxW281
             if(park.position.lon < -119
                 && park.position.lon > -123.039705) {
                 if(park.position.lat > 37.138776
+<<<<<<< HEAD
                     && park.position.lat < 38) {
+=======
+                && park.position.lat < 38) {
+                    // create an object with the following attributes
+                    // (for later reference)
+                    var parkContent = new Location(park.id, park.position.lat, park.position.lon, park.poi.name, park.address.freeformAddress);
+                    console.log(parkContent);
+                    let newLI = new DocumentFragment;
+                    let newListItem = document.createElement('LI');
+                    newListItem.id = parkContent.id;
+                    newListItem.innerHTML = `<h1>${parkContent.name}</h1>
+                    <p class="address">${parkContent.address}</p>
+                    <a class="mapIt" target="_blank" url="http://www.google.com/maps/search/${parkContent.nameUrl}/@${parkContent.lat},${parkContent.lon}">Get Directions</a>`;
+                    newLI.appendChild(newListItem);
+                    list.appendChild(newLI);
+                    // create plot point for park
+>>>>>>> 1dfd9e6 (html list of parks & accompanying placement css)
                     var dogPark = L.circle([park.position.lat, park.position.lon], {
                         color: 'rgba(230, 60, 60, .6)',
                         radius: 0
@@ -37,7 +54,6 @@ fetch("https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=w0Ntsu0zxW281
                     Location(park.id, park.position.lat, park.position.lon, park.poi.name, park.address.freeformAddress);
                 };
             };
-            
         });
     })
     .catch(err => console.log("LOCATION error:", err));
@@ -55,10 +71,9 @@ function Location(id, lat, lon, name, address) {
     this.favoritePark = function() {
         if(this.favorite = false) {
             this.favorite = true;
-        };
-        if(this.favorite = true) {
+        } else if(this.favorite = true) {
             this.favorite = false;
-        }
-    
+        };
     };
-}
+    this.nameUrl = name.replaceAll(" ", "+");
+};
