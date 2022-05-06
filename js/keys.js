50688163c8b4915f9c1ac74bea6636c0cfd28f87
Grey Larrain Bratt <greybratt@outlook.com>
2022-05-06 15:19:55 -0700

    + "keychain" for apis, changed list from UL to OL

---
+++ js/keys.js
@@ -0,0 +1,12 @@
+
+export const Keychain = {
+    // APIs
+    tomtom: new Keys(null, "w0Ntsu0zxW281gaO8nyj33OXcfgQDqbA"),
+    roadPass: new Keys("0c16915f63a0abc852319a41ef180535", "acba3994d3a8c6a52a7dd12361b606f7")      
+};
+
+// constructor
+function Keys(user, pass) {
+    this.user = user;
+    this.pass = pass;
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { Keychain } from "./keys.js";
+
 let headers = new Headers();
 headers.append("X-CSCAPI-KEY", "API_KEY");
 let requestOptions = {
@@ -6,7 +8,6 @@ let requestOptions = {
     redirect: "follow"
 };
 
-const container = document.getElementById("container");
 let list = document.getElementById("parkList");
 
 const map = L.map("map").setView([37.703515, -121.973367], 8);
@@ -17,12 +18,16 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/
     .addTo(map);
 
 
+/*async function getLocation() {
+    let userInput = "";
+    let search = await fetch("")
+}
+fetch("https://api.roadgoat.com/api/v2/destinations/");*/
 
 //fetch("dogpark.json")
-fetch("https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=w0Ntsu0zxW281gaO8nyj33OXcfgQDqbA&limit=100&ofs=0&countryset=US&lat=37.863515&lon=-121.973367&radius=&topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545&language=en-US&categoryset=9362&relatedpois=all")
+fetch(`https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=${Keychain.tomtom.pass}&limit=100&ofs=0&countryset=US&lat=37.863515&lon=-121.973367&radius=&topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545&language=en-US&categoryset=9362&relatedpois=all`)
     .then((response) => response.json())
     .then((pageData) => {
-        console.log(pageData.results);
         let data = pageData.results;
         data.forEach((park) => {
             //topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545
@@ -46,7 +51,7 @@ fetch("https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=w0Ntsu0zxW281
                     // create plot point for park
                     var dogPark = L.circle([park.position.lat, park.position.lon], {
                         color: 'rgba(230, 60, 60, .6)',
-                        radius: 0
+                        radius: 10
                     }).addTo(map)
                     // create pop-up with basic info
                     .bindPopup("<h1>" + park.poi.name + "</h1>" +
