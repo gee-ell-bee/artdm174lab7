e2ec92ab6f15f00c9b4b94f6df3a2a54a99eeb1a
Grey Larrain Bratt <greybratt@outlook.com>
2022-05-06 17:57:42 -0700

    + JS dynamic fetch search, + HTML access in form

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
-import { Keychain } from "./keys.js";
+import { Keychain } from "./keys.js"; // import API keys
 
-let headers = new Headers();
+/*let headers = new Headers();
 headers.append("X-CSCAPI-KEY", "API_KEY");
 let requestOptions = {
     method: "GET",
     headers: headers,
     redirect: "follow"
-};
+};*/
 
 let list = document.getElementById("parkList");
+let place = {
+    name: "Alamo",
+    lat: 37.863515,
+    lon: -121.973367,
 
-const map = L.map("map").setView([37.703515, -121.973367], 8);
+    top: function(lat) {
+        return lat + 0.202553;
+    },
+    left: function(lon) {
+        return lon - 0.484093;
+    },
+    btm: function(lat) {
+        return lat - 0.51;
+    },
+    right: function(lon) {
+        return lon + 0.761822;
+    }
+};
+
+// check data matches for Alamo
+//console.log("topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545");
+//console.log("topLeft=", place.top(place.lat), ",", place.left(place.lon), "&btmRight=", place.btm(place.lat), ",", place.right(place.lon));
+
+const map = L.map("map").setView([place.lat, place.lon], 8);
 L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>",
     subdomains: "abcd",
     maxZoom: 20})
     .addTo(map);
-
 
 /*async function getLocation() {
     let userInput = "";
@@ -25,16 +46,15 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/
 fetch("https://api.roadgoat.com/api/v2/destinations/");*/
 
 //fetch("dogpark.json")
-fetch(`https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=${Keychain.tomtom.pass}&limit=100&ofs=0&countryset=US&lat=37.863515&lon=-121.973367&radius=&topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545&language=en-US&categoryset=9362&relatedpois=all`)
+fetch(`https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=${Keychain.tom.pass}&limit=100&ofs=0&countryset=US&lat=${place.lat}&lon=${place.lon}&topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545&language=en-US&categoryset=9362&relatedpois=all`)
     .then((response) => response.json())
     .then((pageData) => {
         let data = pageData.results;
         data.forEach((park) => {
-            //topLeft=38.066068,-122.457460&btmRight=37.353515,-121.211545
             if(park.position.lon < -119
                 && park.position.lon > -123.039705) {
                 if(park.position.lat > 37.138776
-                && park.position.lat < 38) {
+                && park.position.lat < 38.066068) {
                     // create an object with the following attributes
                     var parkContent = new Location(park.id, park.position.lat, park.position.lon, park.poi.name, park.address.freeformAddress);
                     // create HTML elements
@@ -43,7 +63,7 @@ fetch(`https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=${Keychain.to
                     newListItem.id = parkContent.id;
                     newListItem.innerHTML = `<h1>${parkContent.name}</h1>
                     <p class="address">${parkContent.address}</p>
-                    <a class="mapIt" target="_blank" url="http://www.google.com/maps/search/${parkContent.nameUrl}/@${parkContent.lat},${parkContent.lon}">Get Directions</a>`;
+                    <a class="mapIt" target="_blank" url="https://www.google.com/maps/search/${parkContent.nameUrl}/@${parkContent.lat},${parkContent.lon}">Get Directions</a>`;
                     // append HTML elements
                     newLI.appendChild(newListItem);
                     list.appendChild(newLI);
@@ -51,7 +71,8 @@ fetch(`https://api.tomtom.com/search/2/poiSearch/dog+park.json?key=${Keychain.to
                     // create plot point for park
                     var dogPark = L.circle([park.position.lat, park.position.lon], {
                         color: 'rgba(230, 60, 60, .6)',
-                        radius: 10
+                        radius: 12,
+                        fillColor: 'rgb(230, 60, 60)'
                     }).addTo(map)
                     // create pop-up with basic info
                     .bindPopup("<h1>" + park.poi.name + "</h1>" +
