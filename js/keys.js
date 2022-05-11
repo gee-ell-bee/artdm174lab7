14bbf19f3f56ebd67a0b5ac404b52fcfb1ed6268
Grey Larrain Bratt <greybratt@outlook.com>
2022-05-11 15:28:15 -0700

    - extraneous files, + data credits

--- js/keys.js
+++ js/keys.js
@@ -1,7 +1,6 @@
 export const Keychain = {
     // APIs & associated keys
     tom: new Keys(null, "w0Ntsu0zxW281gaO8nyj33OXcfgQDqbA"),
-    //road: new Keys("0c16915f63a0abc852319a41ef180535", "acba3994d3a8c6a52a7dd12361b606f7")      
 };
 
 // constructor for new keys
